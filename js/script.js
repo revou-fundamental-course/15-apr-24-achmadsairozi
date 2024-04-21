36ddd321bf5b09261cb4277d5dc88a6e6aad5880
Achmad Sairozi <achmad.sairozi@gmail.com>
2024-04-21 21:50:51 +0700

    second commit - add javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 //Ini file js
+
+// Function to convert temperature from Celsius to Fahrenheit
+function celsiusToFahrenheit() {
+    var celsius = parseFloat(document.getElementById("textareaCelsius").value);
+    var fahrenheit = (celsius * 9/5) + 32;
+    
+    document.getElementById("textareaFahrenheit").value = fahrenheit.toFixed(2);
+
+    document.getElementById("textareaKalkulasi").value = '('+celsius+'\u00B0C x 9/5) + 32 = '+fahrenheit.toFixed(2)+'\u00B0F';
+}
+
+// Function to convert temperature from Fahrenheit to Celsius
+function fahrenheitToCelsius() {
+    var fahrenheit = parseFloat(document.getElementById("textareaFahrenheit").value);
+    var celsius = (fahrenheit - 32) * 5/9;
+
+    document.getElementById("textareaCelsius").value = celsius.toFixed(2);
+
+    document.getElementById("textareaKalkulasi").value = '('+fahrenheit+'\u00B0F - 32) * 5/9 = '+celsius.toFixed(2)+'\u00B0C';
+}
+
+function resetTextArea() {
+    document.getElementById("textareaCelsius").value = "";
+    document.getElementById("textareaFahrenheit").value = "";
+    document.getElementById("textareaKalkulasi").value = "";
+}
